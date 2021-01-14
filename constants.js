8fe4987aed8bbe1c71e3ec7088c241427b7e20a8
Brandon Serrao <52131983+brandonserrao@users.noreply.github.com>
2021-01-14 19:59:29 +0100

    Update constants.js
    removed old sheet url; problem was sheet was not shared (was restricted)

--- constants.js
+++ constants.js
@@ -1,6 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-//var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ynT90bScxc8q9NJX3Erk4PGp0Y6-lCq9eqrJRnie1w0/edit#gid=0'; //my custom sheet
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Xcmv168_zDDJujnp8c8lFHbtieyDdkvLQ1fSCETAjbk/edit#gid=164271551';//copy of handsondataviz sheet that seems to allow for CORS
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ynT90bScxc8q9NJX3Erk4PGp0Y6-lCq9eqrJRnie1w0/edit#gid=0'
 
-// insert your own Google Sheets API key from https://console.developers.google.com
+// my Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyDH7_RHe79ajPYJH5hgg-9i97ER-NrpNks';
