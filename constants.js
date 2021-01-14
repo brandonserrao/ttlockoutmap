360d7f513e06e031ee7e78d83c7b12b7c4efbdca
Brandon Serrao <52131983+brandonserrao@users.noreply.github.com>
2021-01-14 19:33:40 +0100

    Update constants.js
    changed to new spreadsheet URL

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Xcmv168_zDDJujnp8c8lFHbtieyDdkvLQ1fSCETAjbk/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ynT90bScxc8q9NJX3Erk4PGp0Y6-lCq9eqrJRnie1w0/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyDH7_RHe79ajPYJH5hgg-9i97ER-NrpNks';
