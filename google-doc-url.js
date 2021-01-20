d664000e42bf4b9fb899074e6502d87691f18723
Brandon Serrao <52131983+brandonserrao@users.noreply.github.com>
2021-01-20 21:08:09 +0100

    added sidebar and map bounding limits

---
+++ google-doc-url.js
@@ -0,0 +1,4 @@
+//link to published google sheets URL from address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ynT90bScxc8q9NJX3Erk4PGp0Y6-lCq9eqrJRnie1w0/edit#gid=0';
+//own google sheets API key
+var googleApiKey = 'AIzaSyDH7_RHe79ajPYJH5hgg-9i97ER-NrpNks';
--- options.js
+++ options.js
@@ -1,9 +1,22 @@
- var pulseIconOptions = {
+var mapOptions = {
+    zoomDelta: 1,
+    center: [-61.22, 10.69],
+    zoom: 2,
+    maxBounds: [[80, -180],
+               [-75, 180]],    
+    //maxBounds: [[85, -Infinity], [-85, Infinity]],//markers do not map to the horizontal space
+    minZoom: 2,
+    maxZoom: 12,
+    markerZoomAnimation: true,
+    maxBoundsViscosity: 0.1,
+    };
+
+var pulseIconOptions = {
             iconSize:[20,20], 
             color:'blue', 
             fillColor:'blue',
             animate: true,
-            heartbeat: 3 //pulse frequency in seconds
+            heartbeat: 2 //pulse frequency in seconds
         };
 
 var pulseIconOptions2 = {
@@ -17,3 +30,25 @@ var clusterMarkerOptions = {
             //iconCreateFunction: function(cluster) {return L.divIcon({ html: '<b>' + cluster.getChildCount() + '</b>' });},//
             iconCreateFunction: function(cluster) {return L.icon.pulse(pulseIconOptions);},//passes a cluster object (like above) and can base icon creation on it's characteristics
         };
+
+var homePanelContent = {
+    id: 'home',
+    tab: '<i class="fa fa-home"></i>',
+    title:'Home Panel',
+    pane:'<h2>Content Header</h2><p>Put your content into here</p>',
+    position: 'top',
+}
+
+var githubButton = {
+    id: 'ghlink',
+    tab: '<i class="fa fa-github"></i>',
+    button:'https://github.com/brandonserrao/legendary-barnacle',
+    position: 'bottom',
+}
+
+var storyTickerContent = {
+    id: 'ticker',
+    tab: '<i class="fa fa-comments"></i>',
+    //title:'Stories',
+    pane:'<h2>Content Header</h2><p>TODO: CREATE UPDATING+SCROLLING TICKER OF STORY SNIPPETS, LINKED TO LOCATION-GOTO LOCATION ONCLICK</p>',
+}
