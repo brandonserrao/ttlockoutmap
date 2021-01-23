9394c3a6e814dd676e981b10301cf557fe0c72c1
Brandon Serrao <52131983+brandonserrao@users.noreply.github.com>
2021-01-24 00:27:09 +0100

    Delete constants.js

--- constants.js
+++
@@ -1,5 +0,0 @@
-// paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ynT90bScxc8q9NJX3Erk4PGp0Y6-lCq9eqrJRnie1w0/edit#gid=0'
-
-// my Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyDH7_RHe79ajPYJH5hgg-9i97ER-NrpNks';
