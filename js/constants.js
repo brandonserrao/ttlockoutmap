c12b7f43a2ddd2f41ac6a9403671a05a41648463
Brandon Serrao <52131983+brandonserrao@users.noreply.github.com>
2021-01-26 19:17:42 +0100

    Add files via upload

--- js/constants.js
+++ js/constants.js
@@ -1,12 +1,12 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ynT90bScxc8q9NJX3Erk4PGp0Y6-lCq9eqrJRnie1w0/edit#gid=0'
+const googleDocURL = 'https://docs.google.com/spreadsheets/d/1ynT90bScxc8q9NJX3Erk4PGp0Y6-lCq9eqrJRnie1w0/edit#gid=0'
 
 //Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyDH7_RHe79ajPYJH5hgg-9i97ER-NrpNks';
+const googleApiKey = 'AIzaSyDH7_RHe79ajPYJH5hgg-9i97ER-NrpNks';
 
 //link to google forms and entry codes for pre-filling; add '&entry.CODESTRING=DESIREDVALUE' at end of BaseURL to prefill desired entries
-var googleFormBaseURL = 'https://docs.google.com/forms/d/e/1FAIpQLSeIZzZ4m6gZqgVPszbrReHtE6g4n0k7gnTGQxKjmx198m0GAA/viewform?usp=pp_url';
-var formEntryCodes = { //to be used for 
+const googleFormBaseURL = 'https://docs.google.com/forms/d/e/1FAIpQLSeIZzZ4m6gZqgVPszbrReHtE6g4n0k7gnTGQxKjmx198m0GAA/viewform?usp=pp_url';
+const formEntryCodes = { //to be used for 
     city : '951397439',
     coords : '1691643040',
     name: '1075366382',
@@ -15,13 +15,13 @@ var formEntryCodes = { //to be used for
 }
 
 //control of snippet length
-var snippetLength = 180 ;//based on twitter old length
-var snippetsTotal = 50; //number of snippets to load
-var scrollTimeIncr = 5000 //time before scrolling to next snippet; not used, change manually in (my_)ticker.js .tick
+const snippetLength = 180 ;//based on twitter old length
+const snippetTotal = 50; //number of snippets to load
+const scrollTimeIncr = 5000 //time before scrolling to next snippet; not used, change manually in (my_)ticker.js .tick
 
 //var tickerContainerId = 'ticker-wrapper-inner'
-var tickerContainerId = 'ticker-wrapper-inner'
+const tickerContainerId = 'ticker-wrapper-inner'
 
-var selectorOn = false; //testing draggable location selector
+const selectorOn = false; //testing draggable location selector
 
 
